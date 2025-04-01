6f2907f50ffd1e291b8c3968aa7252a43bab5861
phael77 <rapha555lima@gmail.com>
2025-03-31 22:29:06 -0300

    ,

--- atividade_aed/arvores/arvore.h
+++ atividade_aed/arvores/arvore.h
@@ -1,13 +1,13 @@
-typedef struct arv Arv;
+    typedef struct arv Arv;
 
-Arv* arv_criavazia();
+    Arv* arv_criavazia();
 
-Arv* arv_cria(char c, Arv* e, Arv* d);
+    Arv* arv_cria(char c, Arv* e, Arv* d);
 
-int arv_vazia(Arv* a);
+    int arv_vazia(Arv* a);
 
-int arv_pertence(Arv* a, char c);
+    int arv_pertence(Arv* a, char c);
 
-Arv* arv_libera(Arv* a);
+    Arv* arv_libera(Arv* a);
 
-void arv_imprime(Arv* a);
+    void arv_imprime(Arv* a);
